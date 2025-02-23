4a0701cfbdadb3b30349743911d6015584351e6f
Flora Demeter <p2681686@my365.dmu.ac.uk>
2025-02-23 21:15:33 +0000

    finalising jobs designs

--- demeterbootsapp/src/features/jobs/jobsList.tsx
+++ demeterbootsapp/src/features/jobs/jobsList.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { JobTable } from '../../components/elements/table';
 
 const JobsList: React.FC = () => {
-    return (
-        <div>
-            A Jobs list
-        </div>
-    )
-
+   
+       const dummyJobsData = [
+           { 'Task ID': 'J00000001', 'Employee Name': 'John Doe', Status: 'Preprocessing' },
+           { 'Task ID': 'J00000002', 'Employee Name': 'Jane Doe', Status: 'Finished' }
+         ];
+   
+       const [jobs, setJobs] = useState(dummyJobsData);
+   
+       const fetchJobs = async () => {
+           try {
+               setJobs(dummyJobsData);
+           } catch (error) {
+               console.error("Error fetching jobs:", error);
+           }
+       };
+   
+       useEffect(() => {
+           fetchJobs();
+       }, []);
+         
+       return (
+           <div className="job-list">
+               <JobTable data={jobs}/>
+           </div>
+       )
 }
 
 export default JobsList;
